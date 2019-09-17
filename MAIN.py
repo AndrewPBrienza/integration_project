cf0d37c2ee91a70246d928e72f19c29b445b870d
AndrewPBrienza <54415403+AndrewPBrienza@users.noreply.github.com>
2019-09-17 13:38:38 -0400

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,3 +1,5 @@
 # Andrew Brienza
-#  Somthing that we dont know yet.
-Print (" Welcome, to the most useless quiz you will ever take")
+#  Somthing that I dont know yet.
+Print (" Welcome User!")
+Print (" Let us take a trip")
+Print (552+632-786*2/3)
